7d8e15f0a48809230b155b9169ec7ed8ac82dea5
Kingsley Victor <javaprodigy56@gmail.com>
2022-09-07 23:07:19 +0100

    add cors

--- src/index.ts
+++ src/index.ts
@@ -73,6 +73,12 @@ router.get('/tokens', async (req, res) => {
   }
 });
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 app.use(morgan('combined'));
 app.use('/', router);
 
